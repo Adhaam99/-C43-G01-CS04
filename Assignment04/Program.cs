598b9d2ae3fa945756ded8ed657aaeac2874f1b8
adham yehia <adhamyehia194@gmail.com>
2024-12-20 22:50:42 +0200

    Q1 - Write a program that allows the user to insert an integer then print all numbers between 1 to that number.

--- Assignment04/Program.cs
+++ Assignment04/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Assignment04
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Assignment04
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Q1 - Write a program that allows the user to insert an integer then print all numbers between 1 to that number.
+
+            Console.Write("Enter as number : ");
+
+            int.TryParse(Console.ReadLine(), out int num);
+
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine(i + 1 );
+            }
+
+            #endregion
+
+            #region Q2 - Write a program that allows the user to insert an integer thenprint a multiplication table up to 12.
+
+
+
+            #endregion
         }
     }
 }
